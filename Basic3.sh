7c192e708b27cb7ff563566ae8b45319f9fb9904
shree <harrynepal@gmail.com>
2020-08-20 09:07:01 +0300

    File is modified.

--- Basic3.sh
+++ Basic3.sh
@@ -1,4 +1,4 @@
-read -p "Enter your name: " username  #
+read -p "Enter your name: " username  # -p means in the same line. 
 read -sp "password: " passcode # -s ,means do not echo input coming from a terminal
 echo
 echo "Name : " username
