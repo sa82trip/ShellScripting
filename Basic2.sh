c0e2cec1d89c6908dbfd127e9f5662468ab6f022
Shree Sapkota <44023155+haaruhito@users.noreply.github.com>
2020-09-21 20:28:39 +0300

    Delete Basic2.sh

--- Basic2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Enter name: " # the names entered by the user is stored in name1 and name2
-
-read name1 name2	#Takes 2 inputs from terminal. User writes the 2 names in the same line.
-
-echo "The entered names are :" $name1, $name2
-
