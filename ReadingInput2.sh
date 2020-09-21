e09bcb20c2c88d3f3156115be91173c751c396de
shree <harrynepal@gmail.com>
2020-09-21 20:20:05 +0300

    Renamed files.

---
+++ ReadingInput2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+read -p "Enter your name: " username  # -p means in the same line. 
+read -sp "password: " passcode # -s ,means do not echo input coming from a terminal
+echo
+echo "Name : " username
+
